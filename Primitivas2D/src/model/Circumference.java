6d680eb4eb91315326b4d2fa79c7d766148a7784
rndae <rndae@users.noreply.github.com>
2017-04-07 21:56:13 -0400

    Update Circumference.java

--- Primitivas2D/src/model/Circumference.java
+++ Primitivas2D/src/model/Circumference.java
@@ -1,14 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.awt.Point;
+
 public class Circunferencia {
 	
-	public ArrayList<Point> polar(int h, int k, int r){     //oso polar :^)
-        ArrayList<Point> poi = new ArrayList<Point>();
+	public ArrayList<Point> circuloP(int h, int k, int r){
+        ArrayList<Point> p = new ArrayList<Point>();
         final double lim = Math.PI/4;
         int x = r, y = 0;
         double ang = 0, inc = 1.0/r;
-        Set<Point> p = new HashSet<Point>();
+        p.add(new Point(h+x, k+y)); //1er
+        p.add(new Point(h-y, k+x)); //3er
+        p.add(new Point(h-x, k-y)); //5to
+        p.add(new Point(h+y, k-x)); //7mo
+        ang = ang + inc;
         while(ang < lim){
+            x = (int)Math.round(r*Math.cos(ang));
+            y = (int)Math.round(r*Math.sin(ang));
             p.add(new Point(h+x, k+y)); //1er
             p.add(new Point(h-x, k+y)); //4to
             p.add(new Point(h-x, k-y)); //5to
@@ -18,10 +29,7 @@ public class Circunferencia {
             p.add(new Point(h-y, k-x)); // 6to
             p.add(new Point(h+y, k-x)); // 7mo
             ang = ang + inc;
-            x = (int)Math.round(r*Math.cos(ang));
-            y = (int)Math.round(r*Math.sin(ang));
         }
-        poi.addAll(p);
-        return poi;
+        return p;
     }
 }
