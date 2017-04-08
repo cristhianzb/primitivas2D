1156d5e866ce90d06eb389c59a7f80229bc9a083
rndae <rndae@users.noreply.github.com>
2017-04-07 22:20:11 -0400

    Update Line.java

--- Primitivas2D/src/model/Line.java
+++ Primitivas2D/src/model/Line.java
@@ -12,6 +12,60 @@ public class Line {
 		this.p2=p2;
 	}
 	
+	public ArrayList<Point> bresenham(int x0, int y0, int x1, int y1){
+        ArrayList<Point> arr = new ArrayList<Point>();        
+        int  ix, iy, x, y, dx, dy, p, deltaA, deltaB;
+        dx = Math.abs(x1 - x0);
+        dy = Math.abs(y1 - y0);
+        x = x0; y = y0;
+        
+        if (x1 >= x0) ix = 1;
+        else ix = -1;
+        
+        if(y1 >= y0) iy = 1;
+        else iy = -1;
+        
+        /*1er/4to / 5to/8vo octante] */
+        if(dx >= dy){                   
+            p = 2*dy - dx;            
+            deltaA = 2*dy;
+            deltaB = 2*(dy-dx);
+            while ((x >= x1 && ix== -1 )|| (x <= x1 && ix == 1 ))
+            {
+                arr.add(new Point(x,y));
+                System.out.println(x+" "+y);
+                x = x + ix;
+                if (p >= 0){
+                    y = y + iy;
+                    p = p + deltaB;
+                }
+                else {
+                    p = p + deltaA;
+                }
+            }
+        }
+        /*2do/3ro / 6to/7mo octante] */
+        else{      
+            p = 2*dx - dy;            
+            deltaA = 2*dx;
+            deltaB = 2*(dx-dy);
+            while ((y >= y1 && iy== -1 )|| (y <= y1 && iy == 1 ))
+            {
+                arr.add(new Point(x,y));
+                System.out.println(x+" "+y);
+                y = y + iy;
+                if (p >= 0){
+                    x = x + ix;
+                    p = p + deltaB;
+                }
+                else {
+                    p = p + deltaA;
+                }
+            }
+        }
+        return arr;
+        }
+	
 	public ArrayList<Point> DDA(Point p1,Point p2){
 		ArrayList<Point> list = new ArrayList<Point>();
 		int dx = p2.x-p1.x;
