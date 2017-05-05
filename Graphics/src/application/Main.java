4aece021770495b2883fc4a05b94631980064d8c
cristhianzb <cristhianzentenob@gmail.com>
2017-05-05 19:22:29 -0400

    Graphics
    Es la segunda parte del proyecto de graficacion

---
+++ Graphics/src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Computer Graphics");
+		
+		StackPane root = new StackPane();
+		Scene scene= new Scene(root,500,200);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
